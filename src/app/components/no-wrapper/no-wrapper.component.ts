8f9ecbbf1aa9daf9ea1ef5a3b57c079bc56a7fe9
Piotr_Starzec <piotr_starzec@epam.com>
2020-11-19 16:26:39 +0100

    host with wrapper example

--- src/app/components/no-wrapper/no-wrapper.component.ts
+++ src/app/components/no-wrapper/no-wrapper.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 
 @Component({
   selector: 'abp-no-wrapper',
   templateUrl: './no-wrapper.component.html',
   styleUrls: ['./no-wrapper.component.scss'],
+  host: {
+    class: 'my-class',
+  },
 })
 export class NoWrapperComponent implements OnInit {
+
+  @HostBinding('class.host-bin-class')
+  private bin = true;
 
   constructor() { }
 
