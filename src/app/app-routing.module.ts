5b8dea557a8c4b65ba4a5343e6e1cfe2f48e951c
Piotr Starzec <piotr_starzec@epam.com>
2020-11-25 16:46:28 +0100

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'reuse-host-component', loadChildren: () => import('src/app/pages/re-use-host').then(m => m.ReUseHostModule),
+    path: 'reuse-host-component', loadChildren: () => import('./pages/re-use-host').then(m => m.ReUseHostModule),
   },
   {
-    path: 'transclusions', loadChildren: () => import('src/app/pages/transclusions').then(m => m.TransclusionsModule),
+    path: 'transclusions', loadChildren: () => import('./pages/transclusions').then(m => m.TransclusionsModule),
   },
   {
-    path: 'dynamic-rendering', loadChildren: () => import('src/app/pages/dynamic-rendering').then(m => m.DynamicRenderingModule),
+    path: 'dynamic-rendering', loadChildren: () => import('./pages/dynamic-rendering').then(m => m.DynamicRenderingModule),
   },
 ];
 
